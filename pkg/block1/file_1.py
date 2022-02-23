9785785cd6f16f08039c7ac39aa18631f78a37e9
DESKTOP-FEH0KJ3\alfre <alfredo.e.p5787@gmail.com>
2022-02-23 07:06:56 -0600

    first python file within block 1

---
+++ pkg/block1/file_1.py
@@ -0,0 +1,10 @@
+# this is the first python file used in this first block
+
+def hello_block1():
+    print("Hello from block1")
+
+def main():
+    hello_block1()
+
+if __name__ == '__main__':
+    main()
